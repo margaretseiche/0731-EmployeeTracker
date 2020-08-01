6fffde4c095211266d5296570e38eed8ef4dc2bf
Margaret Seiche <margaretseiche@hotmail.com>
2020-08-01 08:22:29 -0400

    initial database structure and mysql connection

---
+++ seed.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employeeTrackerDB;
+CREATE DATABASE employeeTrackerDB;
+USE employeeTrackerDB;
+
+CREATE TABLE department (
+  id INTEGER(3) NOT NULL AUTO_INCREMENT, 
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);  
+
+CREATE TABLE role (
+  id INTEGER(3) NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
+  department_id INTEGER(3) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INTEGER(3) NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER(3) NOT NULL,
+  manager_id INTEGER(3), 
+  PRIMARY KEY (id)
+);  
